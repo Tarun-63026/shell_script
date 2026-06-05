8b0f73e7a4619d656c3b48b7ad1be6c1eff8103f
Tarun-63026 <lukalapunarru680@gmail.com>
2026-06-05 23:02:30 +0530

    shell_script

---
+++ 04-array.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+Movies=(
+    "The Shawshank Redemption"
+    "The Godfather"
+    "The Dark Knight"
+    "Pulp Fiction"
+    "The Lord of the Rings: The Return of the King"
+)
+
+echo "My First movie is ${Movies[0]}"
+echo "My Favorite movie is ${Movies[02]}"
