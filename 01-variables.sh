a49170b97e6c92f15f9cbe040a5690a64bdea4a9
Tarun-63026 <lukalapunarru680@gmail.com>
2026-06-05 22:45:45 +0530

    shell_script

---
+++ 01-variables.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+person1="Tarun"
+person2="Kavya"
+
+echo "$person1 :: Hello, $person2!"
+echo "$person2 :: Hi $person1, how are you?"
+echo "$person1 :: I am doing great, how is your work going?"
+echo "$person2 :: Well and Good, but I am think to learn Devops"
