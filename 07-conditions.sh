bbe05da2edb5870e1c2f9f6d9c8aea90358745c8
Tarun-63026 <lukalapunarru680@gmail.com>
2026-06-07 19:55:36 +0530

    Final commit

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -5,6 +5,5 @@ NUMBER=$1
 if [$NUMBER -gt 10]; then
    echo "The given number is greater than 10"
 else
-    echo "The Given number is smaller than 10"
+   echo "The Given number is smaller than 10"
 fi
-
