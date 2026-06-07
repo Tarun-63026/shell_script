8e7ff21e44d8805a876774fa8a542403b664745e
Tarun-63026 <lukalapunarru680@gmail.com>
2026-06-07 23:32:28 +0530

    Final commit

---
+++ 11-colors.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+USERID=$( id -u )
+TIME_STAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOGFILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
+
+R=\e[31m
+G=\e[32m
+N=\3[0m
+
+echo "Your script is running at $TIME_STAMP"
+
+VALIDATE () {
+    if [ $1 -ne 0 ]; then
+       echo " $2 -e $R Failure.. $N"
+    else
+       echo " $2 -e $G success.. $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]; then
+   echo "Please switch to the super user"
+   exit 1
+else
+   echo "You are super user"
+fi
+
+dnf install mysql -y &>> $LOGFILE
+VALIDATE $? "Installation of mysql"
+
+dnf install git -y &>> $LOGFILE
+VALIDATE $? "Installation of git"
+
+echo "Thank you!"
