3667473729338f36bd4b9d2b6c4bd690dd5fea83
Tarun-63026 <lukalapunarru680@gmail.com>
2026-06-07 19:42:28 +0530

    Final commit

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -2,8 +2,8 @@
 
 NUMBER=$1
 
-if [$NUMBER -gt 10]
+if [ $NUMBER -gt 10 ]; then
     echo "The number is greater than 10"
 else
-    echo "Then jnumber is less than 10"\
+    echo "The number is less than 10"
 fi
