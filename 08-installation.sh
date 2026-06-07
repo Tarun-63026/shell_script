d5f3fd156c4c2ec166a55bcd20f011680c1a87d4
Tarun-63026 <lukalapunarru680@gmail.com>
2026-06-07 20:50:20 +0530

    Final commit

--- 08-installation.sh
+++ 08-installation.sh
@@ -10,3 +10,20 @@ else
 fi
 
 dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+   echo "Your mysql installation was failed..."
+else
+   echo "Your mysql insatlltion was success.."
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]; then
+   echo "Your  git was failed..."
+else
+   echo "Your git insatlltion was success.."
+fi
+
+echo "Thank you!"
+
