96d5f5be0039f8fd98771b9b99c2a9a2ce28a3c0
Tarun-63026 <lukalapunarru680@gmail.com>
2026-06-07 19:25:40 +0530

    Final commit

---
+++ 06-special_variables.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "Print all variables : $"
+echo "Number of variables : $#"
+echo "Script name : $0"
+echo "Home directory of the user is : $HOME"
+echo "Current working directory : $PWD"
+echo "Which user is running the script : $USER"
+echo "Hostname of the syste, : $HOSTNAME"
+echo "Process ID of the current shell script : $$"
+echo "Prcoess ID of the last background process : $!"
