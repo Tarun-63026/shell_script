fda418507340ca481b4bcce41f6d67b33e6609af
Tarun-63026 <lukalapunarru680@gmail.com>
2026-06-08 15:50:36 +0530

    Final_commit

--- 13-install_packages.sh
+++ 13-install_packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-USERID=$( id -u )
-TIME_STAMP=$( date +F%-H%-M%-S% )
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+USERID=$(id -u)
+TIME_STAMP=$(date +F%-H%-M%-S%)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
 
 
