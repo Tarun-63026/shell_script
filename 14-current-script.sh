95a94ce7872aab77b4a490bf594bdbc9d152b450
Tarun-63026 <lukalapunarru680@gmail.com>
2026-06-22 15:33:25 +0530

    other

--- 14-current-script.sh
+++ 14-current-script.sh
@@ -6,7 +6,7 @@ COURSE="Devops from the current script"
 echo "Variable valus from the current script: $COURSE"
 echo "Process instance ID of the current script: $$"
 
-./15-other-script.sh
+source ./15-other-script.sh
 
 echo "Course from the other script, course: $COURSE"
 
